5b1305fab3f847b9b40facf47508a7d5d118d1e8
michael Zuniga <michaelzunigaa@gmail.com>
2020-04-21 13:08:24 -0700

    changes

---
+++ models/location.js
@@ -0,0 +1,30 @@
+module.exports = function (sequelize, DataTypes) {
+    const Location = sequelize.define("Location", {
+        // Giving the Author model a name of type STRING
+
+
+        locationId: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        cords: {
+            type: DataTypes.GEOMETRY('POINT'),
+            allowNull: false,
+            validate: {
+                isFloat:true
+            }
+        },
+       
+    });
+    Location.associate = function (models) {
+        // Associating Location with Content
+        // When an Location is deleted, also delete any associated Content
+        Location.hasMany(models.Content, {
+            onDelete: "cascade"
+        });
+    };
+
+    return Location;
+    
+};
